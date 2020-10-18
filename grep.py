e062a97312c3e3254fd32402251a2c9d3793a672
Anastasiia Ponizovkina <anastasia-pon@mail.ru>
2020-10-18 23:43:29 +0300

    content fix

--- grep.py
+++ grep.py
@@ -1,4 +1,3 @@
-
 import argparse
 import sys
 
@@ -15,46 +14,6 @@ def grep(lines, params):
 
 
 def parse_args(args):
-    parser = argparse.ArgumentParser(description='This is a simple grep on python')
-    parser.add_argument(
-        '-v', action="store_true", dest="invert", default=False, help='Selected lines are those not matching pattern.')
-    parser.add_argument(
-        '-i', action="store_true", dest="ignore_case", default=False, help='Perform case insensitive matching.')
-    parser.add_argument(
-        '-c',
-        action="store_true",
-        dest="count",
-        default=False,
-        help='Only a count of selected lines is written to standard output.')
-    parser.add_argument(
-        '-n',
-        action="store_true",
-        dest="line_number",
-        default=False,
-        help='Each output line is preceded by its relative line number in the file, starting at line 1.')
-    parser.add_argument(
-        '-C',
-        action="store",
-        dest="context",
-        type=int,
-        default=0,
-        help='Print num lines of leading and trailing context surrounding each match.')
-    parser.add_argument(
-        '-B',
-        action="store",
-        dest="before_context",
-        type=int,
-        default=0,
-        help='Print num lines of trailing context after each match')
-    parser.add_argument(
-        '-A',
-        action="store",
-        dest="after_context",
-        type=int,
-        default=0,
-        help='Print num lines of leading context before each match.')
-    parser.add_argument('pattern', action="store", help='Search pattern. Can contain magic symbols: ?*')
-    return parser.parse_args(args)
 
 
 def main():
